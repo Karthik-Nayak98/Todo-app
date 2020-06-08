10bcc8175d78272ede45d4e5446133b013227f4d
KarthikNayak024 <karunayak63@gmail.com>
2020-06-09 01:49:37 +0530

    Updates

---
+++ precache-manifest.98aeddf61c5c641fe64f4b5fc7ee60d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5c629669380db269440d6411302685",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "e50b01611bfa696e5e54",
+    "url": "/Todo-app/static/css/main.1d094c73.chunk.css"
+  },
+  {
+    "revision": "c0e02e6a0d865c911cf0",
+    "url": "/Todo-app/static/js/2.89624787.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/Todo-app/static/js/2.89624787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50b01611bfa696e5e54",
+    "url": "/Todo-app/static/js/main.09ec0682.chunk.js"
+  },
+  {
+    "revision": "3e189ff11c9f72ea36f0",
+    "url": "/Todo-app/static/js/runtime-main.ec202724.js"
+  }
+]);
