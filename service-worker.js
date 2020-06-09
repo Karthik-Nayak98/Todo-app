c58cad766446709f9174a2db4432c5d4b7edb13a
KarthikNayak024 <karunayak63@gmail.com>
2020-06-09 09:55:10 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Todo-app/precache-manifest.98aeddf61c5c641fe64f4b5fc7ee60d9.js"
+  "/Todo-app/precache-manifest.b1880df27b6bbceeadb3bcd78c47c2a0.js"
 );
 
 self.addEventListener('message', (event) => {
